43b6e62b7e4a4ddbf9c42cde55b7d2f68c89b4fe
khanglv-vuatho <khanglv@vuatho.com>
2024-04-26 11:47:51 +0700

    upload code

---
+++ fleek.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@fleekxyz/cli').FleekConfig} */
+module.exports = {
+  sites: [
+    {
+      slug: 'many-library-deafening',
+      distDir: 'out',
+      buildCommand: 'npm run build',
+    },
+  ],
+}
