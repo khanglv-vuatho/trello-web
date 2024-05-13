aa1f57ca638cc3ba5cfd50b5a76d0680579b17b8
khanglv-vuatho <khanglv@vuatho.com>
2024-05-13 17:01:42 +0700

    update ads

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Providers from '@/components/Providers'
 import dynamic from 'next/dynamic'
+import Head from 'next/head'
 
 const HeaderDynamic = dynamic(() => import('../../(layout)/Header'), {
   ssr: false,
@@ -15,6 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
+      <Head>
+        <meta name='monetag' content='7fa08e5f7f0fc2ab45ef26e4860af257' />
+      </Head>
       <body>
         <Providers>
           <HeaderDynamic />
