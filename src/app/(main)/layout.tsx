813822d421ab7ed9198e86002e283a873124fe71
khanglv-vuatho <khanglv@vuatho.com>
2024-05-13 17:05:30 +0700

    update analyst

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import Providers from '@/components/Providers'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const HeaderDynamic = dynamic(() => import('../../(layout)/Header'), {
   ssr: false,
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <Head>
         <meta name='monetag' content='7fa08e5f7f0fc2ab45ef26e4860af257' />
       </Head>
+      <GoogleTagManager gtmId='G-M7CBSRN0W9' />
       <body>
         <Providers>
           <HeaderDynamic />
