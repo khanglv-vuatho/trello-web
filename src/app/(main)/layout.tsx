145be48859a1fcd493e5f9a9a49ab05ea4b47ff8
khanglv-vuatho <khanglv@vuatho.com>
2024-05-14 13:50:42 +0700

    add ads

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import Providers from '@/components/Providers'
 import dynamic from 'next/dynamic'
 import { GoogleTagManager } from '@next/third-parties/google'
+import Head from 'next/head'
 
 const HeaderDynamic = dynamic(() => import('../../(layout)/Header'), {
   ssr: false,
@@ -17,7 +18,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html>
       <GoogleTagManager gtmId='G-M7CBSRN0W9' />
-
+      <Head>
+        <meta name='monetag' content='bf1b32bb19ba22d925d491d8d77913fa' />
+      </Head>
       <body>
         <Providers>
           <HeaderDynamic />
